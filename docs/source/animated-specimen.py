314d801611fcdbc944ce9cb3dee24acaa85d4cc1
Eli Heuer <eliheuer@gmail.com>
2018-07-17 15:43:41 -0400

    adding ease-in and ease-out functions

--- docs/source/animated-specimen.py
+++ docs/source/animated-specimen.py
@@ -2,15 +2,39 @@
 
 # Import modules:
 import math
+import os
+import glob
 
 # Basic variables  (width, height, ):
 W, H, M, F = 1024, 1024, 32, 32
 
 # Load font and print font info:
-# print(installedFonts(supportsCharacters=None))
-font("fonts/Dosis-vf.ttf")
-for axis, data in listFontVariations().items():
-    print((axis, data))  # Get axis info from font
+# os.chdir('..')
+# print(os.getcwd())
+# os.chdir('..')
+# print(os.getcwd())
+# os.chdir('fonts')
+# print(os.getcwd())
+# for ttf in glob.glob("*.ttf"):
+#     fontfile = os.getcwd()+"/"+ttf
+#     print(fontfile)
+
+# font("ttf")
+# for axis, data in listFontVariations().items():
+#      print((axis, data))  # Get axis info from font
+
+def easeInOutQuad(n):
+    if n < 0.5:
+        return 2 * n**2
+    else:
+        n = n * 2 - 1
+        return -0.5 * (n*(n-2) - 1)
+        
+def easeInQuad(n):
+    return n**2
+
+def easeOutQuad(n):
+    return -n * (n-2)
 
 # Draw a grid from a given increment:
 def grid(inc):
@@ -29,6 +53,7 @@ varWght = 400
 stepUp = 0
 stepDown = 0
 for frame in range(65):
+    print("frame=", frame)
     newPage(W, H)
     fill(1)           # Background color
     rect(0, 0, W, H)  # Draw the background
@@ -36,6 +61,39 @@ for frame in range(65):
     # Draw the grid
     # grid(32)
 
+
+    # if frame in range(0,7):
+    #     varWght = 400
+    # if frame in range(8, 31):
+    #     stepUp += (24/100)/100
+    #     varWght = 400
+    #     print("step up=", stepUp)                                                                      
+    #     newPos = easeInQuad(stepUp)
+    #     varWght += newPos * 500
+        
+    # if frame in range(32,39):
+    #     varWght = 900
+    # if frame in range(40, 64):
+    #     varWght = 900
+    #     pos = (frame/24.0)
+    #     print("pos=", pos)                                                                      
+    #     newPos = easeInQuad(pos)
+    #     varWght += newPos * 500
+
+    #Calculate the weight
+    # if frame < 50:
+    #     pos = (frame/100.0)*2                                                                      
+    #     newPos = easeInQuad(pos)
+    #     varWght += newPos * 500
+    #     if varWght < 400:
+    #         varWght = 400
+    # if frame > 50:
+    #     pos = (frame/100.0)*2                                                                    
+    #     newPos = easeOutQuad(pos)
+    #     varWght += newPos * 500
+    #     if varWght > 900:
+    #         varWght = 900
+    
     # Calculate the weight
     if frame <= 35:
         pass
@@ -49,12 +107,12 @@ for frame in range(65):
         varWght = 900
     if varWght <= 400:
         varWght = 400
-
+    
     fontVariations(wght=varWght)
     print("varWght=", varWght)
+    font("fonts/Dosis-vf.ttf")
     fill(0)
     stroke(None)
-    font("fonts/Dosis-VF.ttf")
     fontSize(96)
 
     # Draw specimen
@@ -64,8 +122,8 @@ for frame in range(65):
     text("123456789 $€¥£¢ ½ ⅔ ", (M-4, (1024-M)-(5*96)))
     text("[]{}()#* ff ft fft ffi ffl ", (M-4, (1024-M)-(6*96)))
     text("ÆÇÐŁÓÔÒÖÕŒØẞÞ ", (M-4, (1024-M)-(7*96)))
-    text("áâàãäåæçðéèêëıíîìïł ", (M-4, (1024-M)-(8*96)))
-    text("ñóòôöõøœþßšúùûüÿýž ", (M-4, (1024-M)-(9*96)))
+    text("áâàäãåæçðéêèëıíîìïł ", (M-4, (1024-M)-(8*96)))
+    text("ñóôòöõøœþßšúùûüÿýž ", (M-4, (1024-M)-(9*96)))
     fill(1, 0, 0)
 
     # Draw font name
@@ -73,8 +131,9 @@ for frame in range(65):
 
     # Draw secondary text
     fontSize(94)
+    # txtTest = "Weight:" % (str(int(varWght))
     text("Weight:", (M-4), (1024-M)-(10*96))
-    text(str(int(varWght)), ((M+270)+varWght/32), (1024-M)-(10*96))
+    text(str(int(varWght)), ((M+270)+varWght/16), (1024-M)-(10*96))
 
 # Save GIF
 saveImage("animated-specimen.gif")
